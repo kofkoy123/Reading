e589566939af8583f305b6ff470a8c22937f99e9
lzrrong <125032854@qq.com>
2018-06-25 17:31:36 +0800

    看书小软件

--- app/src/main/java/com/shixia/reader/MainActivity.java
+++ app/src/main/java/com/shixia/reader/MainActivity.java
@@ -20,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 
     public void openBook(View view) {
